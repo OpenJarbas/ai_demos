9192e4a217473cfe599524e40b08baca76961e3e
jarbasal <jarbasai@mailfence.com>
2019-03-14 20:08:45 +0000

    setup

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='ai_demos',
-    version='0.1',
+    version='0.1.1',
     packages=['ai_demos'],
-    url='',
+    url='https://github.com/JarbasAl/ai_demos',
     license='MIT',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
